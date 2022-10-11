dd534dd8b9fb72c146b9b51daaa7ecdf13df3750
kunal <kunal@kunals-MacBook-Pro.local>
2022-10-11 12:23:49 +0530

    Initial commit

---
+++ components/Header/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from './header.module.css';
+const header = () => {
+  return (
+    <div>header</div>
+  )
+}
+
+export default header
---
+++ components/heroSection/hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from './hero.module.css';
+const HeroSection = () => {
+  return (
+    <div className={classes.hero}>HeroSection</div>
+  )
+}
+
+export default HeroSection
